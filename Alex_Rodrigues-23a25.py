c41cc543f4a617d3b3e75c2e2d51f3e061fe24b9
Alex <Alexzambryandom@gmail.com>
2022-01-07 22:30:38 -0300

    edo

--- Alex_Rodrigues-23a25.py
+++ Alex_Rodrigues-23a25.py
@@ -1,4 +1,4 @@
-
+# ---Declação de classe e seus métodos apartir da linha 11
 class Veiculo:
 	def __init__(self, chassis, marca, modelo, ano, cor, placa) -> None:
 		self.chassis = chassis
@@ -30,51 +30,54 @@ class Veiculo:
 	def desligar(self):
 		print('Você está parando o veiculo e desligando o mesmo ')
 		print('Fim, você parou o carro e deligou o mesmo')
+# ---Declarando lista
+estoque = list()
 
 
-
-if __name__ == '__main__':
-
-	estoque = list()
-
-	def searchCar(marca: str):
-		for car in estoque:
-			car: Veiculo
-			if car.marca == marca:
-				print(f'Placa: {car.placa}, marca: {car.marca}, cor: {car.cor}, chassis: {car.chassis}, ano: {car.ano}, modelo: {car.modelo}')
-
-	def outputCars():
-		for car in estoque:
-			car: Veiculo
-
+# ---Função de pesquisa de carro
+def searchCar(marca: str):
+	for car in estoque:
+		car: Veiculo
+		if car.marca == marca:
 			print(f'Placa: {car.placa}, marca: {car.marca}, cor: {car.cor}, chassis: {car.chassis}, ano: {car.ano}, modelo: {car.modelo}')
+# ---Função para ver as caracteristicas do carro
+def outputCars():
+	for index, car in enumerate(estoque):
+		car: Veiculo
+		print(f'Placa: {car.placa}, marca: {car.marca}, cor: {car.cor}, chassis: {car.chassis}, ano: {car.ano}, modelo: {car.modelo}')
 
-
-	while True:
-		escolha = input('Continuar produzindo?[ s/n ]\n=> ')
-		
-		if 'n' == escolha:
-			break
-
-		elif 's' == escolha:
-			estoque.append(Veiculo(
-				placa=input('Digite a placa do carro: '),
-				marca=input('Digite a marca do carro: '),
-				cor=input('Digite a cor do carro: '),
-				chassis=input('Digite o chassis do carro: '),
-				ano=int(input('Digite o ano do carro: ')),
-				modelo=input('Digite o modelo do carro: '),
-			))
-
-	searchCar(
-		input('Digite a marca do carro: ')
-	)
-
-	print('\n\n')
-
-	outputCars()
-
-	estoque[0].changeColor('Vermelho')
-	
-
-
+# ---Menu inicial
+while True:
+	escolha = int(input('Digite de acordo com a escolha\n1-Fabricar mais carros  2-Pesquisar por um carro especifico  3-Consultar Caractereisticas dos carros \n  4-Trocar cor de um carro   5-Sair:'))
+	if escolha == 1:
+	    while True:	
+			escolha = input('Continuar produzindo?[ s/n ]\n=> ').lower()
+			
+			if 'n' == escolha:
+				break
+			
+			elif 's' == escolha:
+				estoque.append(Veiculo(
+					placa=input('Digite a placa do carro: '),
+					marca=input('Digite a marca do carro: ').capitalize(),
+					cor=input('Digite a cor do carro: ').capitalize(),
+					chassis=input('Digite o chassis do carro: '),
+					ano=int(input('Digite o ano do carro: ')),
+					modelo=input('Digite o modelo do carro: ').capitalize(),
+				))
+			else:
+				print('Escolha inválida!')
+	elif escolha==2:
+		# ---Puxando função para pesquisar o carro
+		searchCar(input('Digite a marca do carro: '))
+	elif escolha==3:
+		# ---Puxando função para consultar as caracteristicas do carro
+		outputCars()
+	elif escolha ==4:
+		# ---Trocando a cor do carro
+		num=int(input('Digite o index do carro'))
+		estoque[num].changeColor(str(input("Digite a nova cor do veículo: ")))
+	elif escolha==5:
+		print('Fim!')
+	else:
+		print('Escolha de operação inválida!')
